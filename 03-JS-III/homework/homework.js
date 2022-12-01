844480f9d109be15168b25049efb4649ed0cefb4
fanelag <fnm_20@hotmail.com>
2022-12-01 11:20:12 +0100

    hw terminada

--- 03-JS-III/homework/homework.js
+++ 03-JS-III/homework/homework.js
@@ -8,31 +8,67 @@ function imprimirSumaNumeros() {
     // Imprime la suma de los números del 1 al 10
     // Pista: usa un acumulador
     // Tu código:
+    var suma = 0;
+    for(var i = 1; 1 <= 10;i++)
+    {
+      suma += i;
+    }
+   return suma;
 }
 
 function encuentraPares(array){
   // Devuelve un arreglo con los pares encontrados
   // en el arreglo de enteros pasado como parámetro
   // Tu código:
+  var newarray =[];
+  var j = 0;
+  for(var i = 0; i < array.length;i++)
+  {
+    if(array[i] % 2 == 0)
+    {
+      newarray[j]=array[i]
+      j++;
+    }
+  }
+  return newarray;
 }
 
 function elevaAlCuadrado(array){
   // Devuelve un arreglo con cada número del array
   // elevado al cuadrado
   // Tu código:
+  for(var i = 0;i < array.length;i++)
+  {
+    array[i] = Math.pow(array[i],2);
+  }
+  return array;
 }
 
 function sumaArray(array){
   // Devuelve el resultado de sumar todos los elementos
   // de un arreglo de enteros dado
   // Tu código:
+  var resultado;
+  for(var i = 0;i < array.length;i++)
+  {
+    resultado += array[i];
+  }
+  return resultado;
 }
 
 function numeroDigitos(num){
   // Devuelve el número de dígitos de un número dado
   // Tu código:
+  var contador=0;
+  newnumber=0;
+  for(var i =0; newnumber > 0;i++)
+  {
+    newnumber = num/10;
+    contador++; 
+  }
+  return contador;
 }
-  
+   
   
   // No modificar nada debajo de esta línea
   // --------------------------------
